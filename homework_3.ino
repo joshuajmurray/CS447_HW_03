4c06e0cede2d8f888ecde0c3c9f45773849cedea
joshua murray <joshuajmurray@gmail.com>
2018-01-29 13:55:48 -0800

    running ave filter test on one input, need to do other

--- homework_3.ino
+++ homework_3.ino
@@ -3,33 +3,27 @@
 
 #define SENSOR_LEFT 0
 #define SENSOR_RIGHT 1
-//#define SENSOR_CENTER 2
 #define LOOP_TIME 50
 #define POT 3
 #define SERVO 9
-
-#define HUNDO 10
-#define TEN 35
-#define SEC 1000
-
 #define LOWER 0
 #define UPPER 180
-#define MIN_TO_MOVE 3
-//#define GAIN 0.1
-#define DEBUG true
-//#define DEBUG false
+#define MIN_TO_MOVE 2
+#define GAIN 0.03
+#define NUM_READINGS 10
+
+int leftAve[NUM_READINGS];      // the readings from the analog input
+int leftIndex;              // the index of the current reading
+int leftTotal;                  // the running total
+int leftAverage;                // the average
 
 Servo myservo;
-unsigned long hundredhzTimer,tenhzTimer,onehzTimer;
 int leftSensor,rightSensor,newVal,angle,error,change;
-float gain;
-bool maxed;
+//float gain;
 
 void setup() {
-  hundredhzTimer,tenhzTimer,onehzTimer = 0;
   leftSensor,rightSensor,newVal,angle,error,change = 0;
-  gain = 0.0;
-  maxed = false;
+  leftIndex,leftTotal,leftAverage = 0;
   pinMode(SENSOR_LEFT, INPUT);
   pinMode(SENSOR_RIGHT, INPUT);
   myservo.attach(SERVO);
@@ -41,38 +35,28 @@ void setup() {
 }
 
 void readWrite() {
-    leftSensor = analogRead(SENSOR_LEFT);
-    rightSensor = analogRead(SENSOR_RIGHT);
-    gain = constrain((map(analogRead(POT),0,1023,0,100)*.001),0.02,0.10);
-    angle = myservo.read();
-    error = leftSensor - rightSensor;//calculate error
-    change = error*gain;
-    newVal = constrain((angle + change),LOWER,UPPER);//constrain the new value so it doesn't try to move past it's limits
-    if(abs(change) > MIN_TO_MOVE) {// && angle < UPPER && angle > LOWER){//if the servro is at the limit of it's travel or the request is below the threhold don't move
-      myservo.write(newVal);
-      maxed = false;
-    } else {
-      maxed = true;
-    }
-    tenhzTimer += TEN;  
+  leftSensor = analogRead(SENSOR_LEFT);
+  rightSensor = analogRead(SENSOR_RIGHT);
+  angle = myservo.read();
+  error = leftAverage - rightSensor;//calculate error
+//  error = leftSensor - rightSensor;//calculate error
+  change = error*GAIN;
+  newVal = constrain((angle + change),LOWER,UPPER);//constrain the new value so it doesn't try to move past it's limits
+  if(abs(change) > MIN_TO_MOVE) {//if the servro is at the limit of it's travel or the request is below the threhold don't move
+    myservo.write(newVal);
+  }
+
+  leftTotal = leftTotal - leftAve[leftIndex];// subtract the last reading  
+  leftAve[leftIndex] = leftSensor;
+  leftTotal = leftTotal + leftAve[leftIndex];// add the reading to the total
+  leftIndex += 1;// advance to the next position in the array
+  
+  if (leftIndex >= NUM_READINGS) {// if we're at the end of the array...
+    leftIndex = 0;// ...wrap around to the beginning
+  }
+
+  leftAverage = leftTotal / NUM_READINGS;// calculate the average
 }
 
 void loop() {
-//  if(onehzTimer < millis() && DEBUG) {//this whole loop is debug code
-//    Serial.println(F("================================="));
-//    Serial.print(F("Servo angle"));Serial.println(myservo.read());
-//    Serial.print(F("Left Sensor: "));Serial.println(leftSensor);
-//    Serial.print(F("Right Sensor: "));Serial.println(rightSensor);
-//    Serial.print(F("Error: "));Serial.println(error);
-//    Serial.print(F("New val: "));Serial.println(newVal);
-//    Serial.print(F("Trim pot: "));Serial.println(analogRead(POT));
-//    Serial.print(F("Gain: "));Serial.println(gain);
-//    Serial.print(F("Change: "));Serial.println(change);
-//    if(maxed){
-//      Serial.println(F("MAXED, no move"));
-//    }else {
-//      Serial.println(F("MOVING"));
-//    }    
-//    onehzTimer += SEC;
-//  }  
 }
