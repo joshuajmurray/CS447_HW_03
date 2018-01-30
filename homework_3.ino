e25d96fe2803c406181b887fb8269d508259ddef
joshua murray <joshuajmurray@gmail.com>
2018-01-29 16:33:31 -0800

    Add running ave to both and test with various gains

--- homework_3.ino
+++ homework_3.ino
@@ -9,17 +9,14 @@
 #define LOWER 0
 #define UPPER 180
 #define MIN_TO_MOVE 2
-#define GAIN 0.03
-#define NUM_READINGS 10
-
-int leftAve[NUM_READINGS];      // the readings from the analog input
-int leftIndex;              // the index of the current reading
-int leftTotal;                  // the running total
-int leftAverage;                // the average
+#define GAIN 0.055
+#define NUM_READINGS 2
 
 Servo myservo;
 int leftSensor,rightSensor,newVal,angle,error,change;
-//float gain;
+int leftIndex,rightIndex,leftTotal,rightTotal,leftAverage,rightAverage;
+int leftAve[NUM_READINGS];      // the readings from the analog input
+int rightAve[NUM_READINGS];      // the readings from the analog input
 
 void setup() {
   leftSensor,rightSensor,newVal,angle,error,change = 0;
@@ -28,8 +25,6 @@ void setup() {
   pinMode(SENSOR_RIGHT, INPUT);
   myservo.attach(SERVO);
   myservo.write(90);
-  Serial.begin(115200);
-  Serial.println(F("Begin"));
   MsTimer2::set(LOOP_TIME, readWrite);
   MsTimer2::start();
 }
@@ -38,17 +33,16 @@ void readWrite() {
   leftSensor = analogRead(SENSOR_LEFT);
   rightSensor = analogRead(SENSOR_RIGHT);
   angle = myservo.read();
-  error = leftAverage - rightSensor;//calculate error
-//  error = leftSensor - rightSensor;//calculate error
+  error = leftAverage - rightAverage;//calculate error
   change = error*GAIN;
   newVal = constrain((angle + change),LOWER,UPPER);//constrain the new value so it doesn't try to move past it's limits
   if(abs(change) > MIN_TO_MOVE) {//if the servro is at the limit of it's travel or the request is below the threhold don't move
     myservo.write(newVal);
   }
-
-  leftTotal = leftTotal - leftAve[leftIndex];// subtract the last reading  
+//============= running ave for left side
+  leftTotal -= leftAve[leftIndex];// subtract the last reading  
   leftAve[leftIndex] = leftSensor;
-  leftTotal = leftTotal + leftAve[leftIndex];// add the reading to the total
+  leftTotal += leftAve[leftIndex];// add the reading to the total
   leftIndex += 1;// advance to the next position in the array
   
   if (leftIndex >= NUM_READINGS) {// if we're at the end of the array...
@@ -56,6 +50,18 @@ void readWrite() {
   }
 
   leftAverage = leftTotal / NUM_READINGS;// calculate the average
+//============= running ave for right side
+  rightTotal -= rightAve[rightIndex];// subtract the last reading  
+  rightAve[rightIndex] = rightSensor;
+  rightTotal += rightAve[rightIndex];// add the reading to the total
+  rightIndex += 1;// advance to the next position in the array
+  
+  if (rightIndex >= NUM_READINGS) {// if we're at the end of the array...
+    rightIndex = 0;// ...wrap around to the beginning
+  }
+
+  rightAverage = rightTotal / NUM_READINGS;// calculate the average
+//=============
 }
 
 void loop() {
