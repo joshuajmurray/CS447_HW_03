a6a9bb2a4f5d44ec488622583822d6a9b2f1f04c
joshua murray <joshuajmurray@gmail.com>
2018-01-29 23:24:30 -0800

    Cleanup running ave code.

--- homework_3/homework_3.ino
+++ homework_3/homework_3.ino
@@ -4,64 +4,39 @@
 #define SENSOR_LEFT 0
 #define SENSOR_RIGHT 1
 #define LOOP_TIME 50
-#define POT 3
 #define SERVO 9
 #define LOWER 0
 #define UPPER 180
 #define MIN_TO_MOVE 2
 #define GAIN 0.055
-#define NUM_READINGS 2
 
 Servo myservo;
-int leftSensor,rightSensor,newVal,angle,error,change;
-int leftIndex,rightIndex,leftTotal,rightTotal,leftAverage,rightAverage;
-int leftAve[NUM_READINGS];      // the readings from the analog input
-int rightAve[NUM_READINGS];      // the readings from the analog input
+int leftSensor,rightSensor,newVal,angle,error,change,leftPrev,rightPrev,leftAve,rightAve;
 
 void setup() {
-  leftSensor,rightSensor,newVal,angle,error,change = 0;
-  leftIndex,leftTotal,leftAverage = 0;
+  leftSensor,rightSensor,newVal,angle,error,change,leftPrev,rightPrev = 0;
   pinMode(SENSOR_LEFT, INPUT);
   pinMode(SENSOR_RIGHT, INPUT);
   myservo.attach(SERVO);
-  myservo.write(90);
+  myservo.write(90);//init to center
   MsTimer2::set(LOOP_TIME, readWrite);
   MsTimer2::start();
 }
 
 void readWrite() {
-  leftSensor = analogRead(SENSOR_LEFT);
-  rightSensor = analogRead(SENSOR_RIGHT);
-  angle = myservo.read();
-  error = leftAverage - rightAverage;//calculate error
-  change = error*GAIN;
+  leftSensor = constrain(analogRead(SENSOR_LEFT),1,1023);//constrain to avoid divide by zero error
+  rightSensor = constrain(analogRead(SENSOR_RIGHT),1,1023);
+  leftAve = ((leftSensor+leftPrev)/2);//simple running average with minimal history
+  rightAve = ((rightSensor+rightPrev)/2);
+  leftPrev = leftSensor;//save for next iteration
+  rightPrev = rightSensor;
+  angle = myservo.read();//get current position
+  error = leftAve - rightAve;//calculate error
+  change = error*GAIN;//apply gain
   newVal = constrain((angle + change),LOWER,UPPER);//constrain the new value so it doesn't try to move past it's limits
-  if(abs(change) > MIN_TO_MOVE) {//if the servro is at the limit of it's travel or the request is below the threhold don't move
+  if(abs(change) > MIN_TO_MOVE) {//if the change is below the threhold don't move
     myservo.write(newVal);
   }
-//============= running ave for left side
-  leftTotal -= leftAve[leftIndex];// subtract the last reading  
-  leftAve[leftIndex] = leftSensor;
-  leftTotal += leftAve[leftIndex];// add the reading to the total
-  leftIndex += 1;// advance to the next position in the array
-  
-  if (leftIndex >= NUM_READINGS) {// if we're at the end of the array...
-    leftIndex = 0;// ...wrap around to the beginning
-  }
-
-  leftAverage = leftTotal / NUM_READINGS;// calculate the average
-//============= running ave for right side
-  rightTotal -= rightAve[rightIndex];// subtract the last reading  
-  rightAve[rightIndex] = rightSensor;
-  rightTotal += rightAve[rightIndex];// add the reading to the total
-  rightIndex += 1;// advance to the next position in the array
-  
-  if (rightIndex >= NUM_READINGS) {// if we're at the end of the array...
-    rightIndex = 0;// ...wrap around to the beginning
-  }
-
-  rightAverage = rightTotal / NUM_READINGS;// calculate the average
-//=============
 }
 
 void loop() {
